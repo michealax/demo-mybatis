63e594a66a82effcf4b4185444fd26fb92d8eade
shane <michealax@github.com>
2024-06-08 16:13:18 +0800

    add spring schedule

--- src/main/java/com/shane/mybatis/MybatisDemoApplication.java
+++ src/main/java/com/shane/mybatis/MybatisDemoApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.shane.mybatis.repository")
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableScheduling
 public class MybatisDemoApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/shane/mybatis/schedule/CacheUpdater.java
@@ -0,0 +1,20 @@
+package com.shane.mybatis.schedule;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+@Slf4j
+public class CacheUpdater {
+
+    @Scheduled(fixedRate = 5000)
+    public void runScheduleFixedRate(){
+        log.info("update cache in fixed rate");
+    }
+
+    @Scheduled(cron = "0/5 * * * * *")
+    public void runScheduleCron(){
+        log.info("update cache every 5 seconds");
+    }
+}
