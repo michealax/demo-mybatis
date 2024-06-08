b4e0b3fb40a2a0d74f9353294e8e1754bf8eca61
shane <michealax@github.com>
2024-06-08 17:02:01 +0800

    add local quartz

---
+++ src/main/java/com/shane/mybatis/config/QuartzConfig.java
@@ -0,0 +1,29 @@
+package com.shane.mybatis.config;
+
+import com.shane.mybatis.schedule.HelloJob;
+import org.quartz.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuartzConfig {
+
+    @Bean("helloJob")
+    public JobDetail helloJobDetail() {
+        return JobBuilder.newJob(HelloJob.class)
+                .withIdentity("DateTimeJob")
+                .usingJobData("msg", "hello quartz")
+                .storeDurably()
+                .build();
+    }
+
+    @Bean
+    public Trigger printTimeTrigger() {
+        CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule("0/1 * * * * ?");
+        return TriggerBuilder.newTrigger()
+                .forJob(helloJobDetail())
+                .withIdentity("quartzTaskService")
+                .withSchedule(cronScheduleBuilder)
+                .build();
+    }
+}
---
+++ src/main/java/com/shane/mybatis/schedule/HelloJob.java
@@ -0,0 +1,19 @@
+package com.shane.mybatis.schedule;
+
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import java.util.Date;
+
+@Slf4j
+public class HelloJob extends QuartzJobBean {
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        context.getJobDetail().getJobDataMap().forEach(
+                (k, v) -> log.info("param, key:{}, value: {}", k, v)
+        );
+        log.info("Hello Job execution time: " + new Date());
+    }
+}
